0a60341e93900548482e235b9d18c9ce4239d06f
millerding <mding022@uottawa.ca>
2024-09-29 02:34:35 -0400

    Add cors

--- backend/src/main/java/com/millerding/p2p/controller/ChunkBuilderController.java
+++ backend/src/main/java/com/millerding/p2p/controller/ChunkBuilderController.java
@@ -7,13 +7,15 @@ import java.nio.file.Files;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
+import org.springframework.web.bind.annotation.RestController;
 import org.json.JSONObject;
 import org.json.JSONException;
 
-
+@RestController
+@CrossOrigin(origins = "localhost:3000")
 public class ChunkBuilderController {
 
     @GetMapping("/build")
